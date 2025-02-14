f916ede00bd533d7d5a74191b751b1a4c61cabae
Aloento <11802769+Aloento@users.noreply.github.com>
2025-02-14 23:12:53 +0100

    [feat] 添加数据表组件，更新页面布局，新增 Prisma 配置和环境变量

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import { Card } from "antd";
+import { Card, Table } from "antd";
+
+interface IDatasetList {
+  id: number;
+  name: string;
+  items: number;
+}
 
 export default function Home() {
-  return (
+  return <>
     <Card>
       123
     </Card>
-  );
+
+    <Table>
+
+    </Table>
+  </>;
 }
