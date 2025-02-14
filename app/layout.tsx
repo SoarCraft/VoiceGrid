b867f2fd255087fe27d5df1b3563e6250c0f4fb6
Aloento <11802769+Aloento@users.noreply.github.com>
2025-02-14 01:37:46 +0100

    [feat] 更新布局，添加侧边栏和页脚，移除不必要的配置，优化页面结构

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
 import { AntdRegistry } from "@ant-design/nextjs-registry";
+import { Layout, Menu } from "antd";
+import { Content, Footer, Header } from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
 import type { Metadata } from "next";
 import "./globals.css";
 
@@ -12,7 +15,34 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     <html lang="zh-Hans">
       <body className="antialiased">
         <AntdRegistry>
-          {children}
+          <Layout className="!min-h-screen antialiased">
+            <Sider theme="light" collapsible>
+              <div className="text-center py-4 text-xl">VoiceGrid</div>
+
+              <Menu defaultSelectedKeys={['1']} mode="inline" items={[
+                {
+                  key: "1",
+                  label: "Datasets",
+                }
+              ]} />
+            </Sider>
+
+            <Layout>
+              <Header className="!bg-white">
+                123
+              </Header>
+
+              <Content className="m-4">
+                <div className="p-6 min-h-80 bg-white rounded-lg">
+                  {children}
+                </div>
+              </Content>
+
+              <Footer className="text-center">
+                SoarCraft ©{new Date().getFullYear()} Part of CheersYou
+              </Footer>
+            </Layout>
+          </Layout>
         </AntdRegistry>
       </body>
     </html>
