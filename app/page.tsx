efbc2aac7fc085504b53328a75fd8f47be9e4659
Aloento <11802769+Aloento@users.noreply.github.com>
2025-02-15 00:00:07 +0100

    [feat] 更新数据表组件，添加标签列

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
-import { Card, Table } from "antd";
+import { Card, Table, Tag } from "antd";
+
+const { Column } = Table;
 
 interface IDatasetList {
-  id: number;
+  key: number;
   name: string;
   items: number;
+  tags: string[];
 }
+
+const mock: IDatasetList[] = [
+  {
+    key: 1,
+    name: "dataset1",
+    items: 100,
+    tags: ["tag1", "tag2"],
+  },
+  {
+    key: 2,
+    name: "dataset2",
+    items: 200,
+    tags: ["tag1", "tag3"],
+  },
+];
 
 export default function Home() {
   return <>
@@ -12,8 +30,23 @@ export default function Home() {
       123
     </Card>
 
-    <Table>
+    <Table<IDatasetList> dataSource={mock}>
+      <Column title="Id" dataIndex="key" key="key" />
+      <Column title="Name" dataIndex="name" key="name" />
+      <Column title="Items" dataIndex="items" key="items" />
 
+      <Column
+        title="Tags"
+        dataIndex="tags"
+        key="tags"
+        render={(tags: string[]) => <>
+          {tags.map((tag) => (
+            <Tag key={tag}>
+              {tag}
+            </Tag>
+          ))}
+        </>}
+      />
     </Table>
   </>;
 }
