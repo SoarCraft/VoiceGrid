11bd18eb92e7b24d03cb310957afdbcff88c2d54
Aloento <11802769+Aloento@users.noreply.github.com>
2025-02-18 21:22:07 +0100

    [feat] 添加数据集详情组件mock，重构主页以使用新组件，移除旧的表格实现

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import { Card, Table, Tag } from "antd";
-
-const { Column } = Table;
+import { DatasetDetail } from "@/components/DatasetDetail";
 
 interface IDatasetList {
   key: number;
@@ -9,44 +7,6 @@ interface IDatasetList {
   tags: string[];
 }
 
-const mock: IDatasetList[] = [
-  {
-    key: 1,
-    name: "dataset1",
-    items: 100,
-    tags: ["tag1", "tag2"],
-  },
-  {
-    key: 2,
-    name: "dataset2",
-    items: 200,
-    tags: ["tag1", "tag3"],
-  },
-];
-
 export default function Home() {
-  return <>
-    <Card>
-      123
-    </Card>
-
-    <Table<IDatasetList> dataSource={mock}>
-      <Column title="Id" dataIndex="key" key="key" />
-      <Column title="Name" dataIndex="name" key="name" />
-      <Column title="Items" dataIndex="items" key="items" />
-
-      <Column
-        title="Tags"
-        dataIndex="tags"
-        key="tags"
-        render={(tags: string[]) => <>
-          {tags.map((tag) => (
-            <Tag key={tag}>
-              {tag}
-            </Tag>
-          ))}
-        </>}
-      />
-    </Table>
-  </>;
+  return <DatasetDetail />;
 }
